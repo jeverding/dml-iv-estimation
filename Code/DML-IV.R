cdd15313b1c87d21d476deea8e930827deea4e6a
jeverding <jakob.everding@uni-hamburg.de>
2020-03-23 21:29:40 +0100

    Elastic net DML estimation

--- Code/DML-IV.R
+++ Code/DML-IV.R
@@ -60,10 +60,10 @@ clust.se <- function(est.model, cluster){
 # Partialling out (first part of DML algorithm) 
 partial.out <- function(y, x, nfold = 5){
   # Setting up the table 
-  columns <- c("MSE", 
-               "lambda", "alpha", # For elastic net 
-               "mtry", "ntree", # For random forest (ranger) 
-               "iterations", "learn_rate", "max_depth", "gamma", "frac_subsample", "colsample_bytree" # For gradient boosting (xgb) 
+  columns <- c("MSE" 
+               , "lambda", "alpha" # For elastic net 
+               #, "mtry", "ntree" # For random forest (ranger) 
+               #, "iterations", "learn_rate", "max_depth", "gamma", "frac_subsample", "colsample_bytree" # For gradient boosting (xgb) 
                )  
   table.mse <- data.frame(matrix(nrow=0, ncol= length(columns)))
   colnames(table.mse) <- columns
@@ -85,7 +85,7 @@ partial.out <- function(y, x, nfold = 5){
   
   #### Elastic Net (incl. Lasso, Ridge) ####
   # Set step size for alpha 
-  r <- 0.2
+  r <- 0.1
   for (i in seq(0, 1, r)) {
     # 10-fold CV to estimate tuning paramter with the lowest prediction error 
     set.seed(seed.set)
@@ -111,100 +111,7 @@ partial.out <- function(y, x, nfold = 5){
     print(paste("glmnet using alpha =", i, "fitted."))
   }
   
-  #### Random Forest ####
-  # Hyperparameter tuning 
-  rf.grid <- expand.grid(ntree = c(500, 1000, 2000, 5000), 
-                         mtry = c(floor(sqrt(ncol(x.train))*1.5), floor(sqrt(ncol(x.train))), floor(sqrt(ncol(x.train))*0.5), 2) 
-                         ) # max.depth = c(1, 2, 3, 5, 0) 
-  # Start: RF grid search 
-  for(i in 1:nrow(rf.grid)) {
-    set.seed(seed.set)
-    rf <- ranger(x = x.train, y = y.train, 
-                 num.trees = rf.grid$ntree[i], 
-                 mtry = rf.grid$mtry[i]) 
-    pred <- predict(rf, data = x.test, type="response")$predictions 
-    
-    # new NA row for random forest, fill again sequentially 
-    table.mse[dim(table.mse)[1]+1,] <- rep(NA,length(columns)) 
-    rownames(table.mse)[dim(table.mse)[1]]  <- paste0("Random Forest (no. ", i, "/", nrow(rf.grid), ")") 
-    table.mse$MSE[dim(table.mse)[1]] <- mean((pred - y.test)^2) 
-    table.mse$mtry[dim(table.mse)[1]] <- rf.grid$mtry[i] 
-    table.mse$ntree[dim(table.mse)[1]]<- rf.grid$ntree[i] 
-    print(paste0("RF (ranger) no. ", i, "/", nrow(rf.grid), " fitted."))
-  } # End: RF grid search 
-  
-  #### Gradient Boosting (XGB) ####
-  # Hyperparameter tuning 
-  xgb.grid <- expand.grid(nrounds = 20000, 
-                          eta = c(0.01, 0.1, 0.3), #c(0.01, 0.05, 0.1, 0.15, 0.2, 0.3), 
-                          max_depth = c(1, 2, 3, 5), #c(1, 2, 3, 5, 7), 
-                          gamma = 0, 
-                          subsample = 0.75, #c(0.75, 1),
-                          colsample_bytree = 0.8, #c(0.7, 0.8, 0.9, 1), 
-                          opt.trees = NA,               # save results here 
-                          min.RMSE = NA                 # save results here 
-                          )
-  # Start: XGB grid search 
-  for(i in 1:nrow(xgb.grid)) {
-    # Create parameter list
-    params <- list(nrounds = xgb.grid$nrounds[i], 
-                   eta = xgb.grid$eta[i], 
-                   max_depth = xgb.grid$max_depth[i], 
-                   gamma = xgb.grid$gamma[i], 
-                   subsample = xgb.grid$subsample[i], 
-                   colsample_bytree = xgb.grid$colsample_bytree[i]) 
-    
-    # Tune model using 5-fold cv 
-    set.seed(seed.set)
-    xgb.tune <- xgb.cv(params = params, 
-                       data = x.train, 
-                       label = y.train, 
-                       nrounds = params$nrounds, 
-                       nfold = 5, 
-                       objective = "reg:squarederror",  # for regression models 
-                       early_stopping_rounds = 100, # stop if no improvement for 100 consecutive trees 
-                       print_every_n = 500, 
-                       eval_metric = "rmse")
-    
-    # Save results (number of trees/iterations and training error) to grid 
-    xgb.grid$opt.trees[i] <- which.min(xgb.tune$evaluation_log$test_rmse_mean)
-    xgb.grid$min.RMSE[i] <- min(xgb.tune$evaluation_log$test_rmse_mean)
-  } # End: XGB grid search 
-  
-  # Learn final model with optimal parameters on whole training data 
-  # Create parameter list 
-  params <- list(nrounds = xgb.grid$opt.trees[which.min(xgb.grid$min.RMSE)], 
-                 eta = xgb.grid$eta[which.min(xgb.grid$min.RMSE)], 
-                 max_depth = xgb.grid$max_depth[which.min(xgb.grid$min.RMSE)], 
-                 gamma = xgb.grid$gamma[which.min(xgb.grid$min.RMSE)], 
-                 subsample = xgb.grid$subsample[which.min(xgb.grid$min.RMSE)], 
-                 colsample_bytree = xgb.grid$colsample_bytree[which.min(xgb.grid$min.RMSE)]) 
-  # Train final model w/ cross-validated hyperparameters 
-  set.seed(seed.set)
-  xgb.trained <- xgb.train(params = params, 
-                           data = xgb.DMatrix(x.train, 
-                                              label = y.train), 
-                           nrounds = params$nrounds, 
-                           objective = "reg:squarederror",  # for regression models 
-                           eval_metric = "rmse") 
-  # Predict test data using trained final model 
-  pred <- predict(xgb.trained, newdata = x.test) 
-  
-  # new NA row for extreme gradient boosting, fill again sequentially 
-  table.mse[dim(table.mse)[1]+1,] <- rep(NA,length(columns)) 
-  rownames(table.mse)[dim(table.mse)[1]]  <- paste0("Extreme Gradient Boosting") 
-  table.mse$MSE[dim(table.mse)[1]] <- mean((pred - y.test)^2) 
-  table.mse$iterations[dim(table.mse)[1]] <- params$nrounds 
-  table.mse$learn_rate[dim(table.mse)[1]]<- params$eta 
-  table.mse$max_depth[dim(table.mse)[1]]<- params$max_depth 
-  table.mse$gamma[dim(table.mse)[1]]<- params$gamma 
-  table.mse$frac_subsample[dim(table.mse)[1]]<- params$subsample 
-  table.mse$colsample_bytree[dim(table.mse)[1]]<- params$colsample_bytree 
-  print("XGB fitted.") 
-  
-  
-  # Benchmarking: Select best method based on OOB MSE 
-  # (Identify best method directly using method-specific tuning parameters): 
+  # Benchmarking: Select best model based on OOB MSE 
   # Prepare data for (nfold) cross-fitting 
   set.seed(seed.set)
   foldid <- rep.int(1:nfold, times = ceiling(nrow(x)/nfold))[sample.int(nrow(x))]
@@ -215,38 +122,14 @@ partial.out <- function(y, x, nfold = 5){
   for(b in 1:length(I)){
     print(paste0("Compute residuals: Fold ", b, "/", length(I)))
     set.seed(seed.set)
-    # Elnet 
-    if (!is.na(table.mse$lambda[which.min(table.mse$MSE)])) { 
-      opt.alpha <- table.mse$alpha[which.min(table.mse$MSE)]
-      opt.lambda <- table.mse$lambda[which.min(table.mse$MSE)]
-      best.mod <- glmnet(x[-I[[b]],], y[-I[[b]]], 
-                         alpha = opt.alpha, 
-                         lambda = opt.lambda)
-      hat <- predict(best.mod, type = 'response', 
-                       s = opt.lambda, 
-                       newx = x[I[[b]],])
-      }
-    # RF 
-    if (!is.na(table.mse$mtry[which.min(table.mse$MSE)])) { 
-      opt.mtry <- table.mse$mtry[which.min(table.mse$MSE)] 
-      opt.ntree <- table.mse$ntree[which.min(table.mse$MSE)] 
-      best.mod <- ranger(x = x[-I[[b]],], y = y[-I[[b]]], 
-                         num.trees = opt.ntree, 
-                         mtry = opt.mtry) 
-      hat <- predict(best.mod, data = x[I[[b]],], type="response")$predictions 
-      }
-    # XGBoost 
-    # (No need to specify hyperparameters again, as only the best xgb model is written to table. 
-    # Hence, its hyperparameters can still be directly called from params-list.) 
-    if (!is.na(table.mse$learn_rate[which.min(table.mse$MSE)])) { 
-      best.mod <- xgb.train(params = params, 
-                            data = xgb.DMatrix(x[-I[[b]],], 
-                                               label = y[-I[[b]]]), 
-                            nrounds = params$nrounds, 
-                            objective = "reg:squarederror",  # for regression models 
-                            eval_metric = "rmse") 
-      hat <- predict(best.mod, newdata = x[I[[b]],]) 
-      }
+    opt.alpha <- table.mse$alpha[which.min(table.mse$MSE)]
+    opt.lambda <- table.mse$lambda[which.min(table.mse$MSE)]
+    best.mod <- glmnet(x[-I[[b]],], y[-I[[b]]], 
+                       alpha = opt.alpha, 
+                       lambda = opt.lambda)
+    hat <- predict(best.mod, type = 'response', 
+                   s = opt.lambda, 
+                   newx = x[I[[b]],])
     til[I[[b]]] <- (y[I[[b]]] - hat) 
     }
   return(list("til" = til, 
@@ -263,7 +146,7 @@ ctrend_2 <- paste0("trend2cntry_", 1:length(unique(data.share$country)))
 ctrend_3 <- paste0("trend3cntry_", 1:length(unique(data.share$country))) 
 var.select <- c("eurodcat", "chyrseduc", "t_compschool", 
                 "country", "chbyear", "sex", "chsex", "int_year", "agemonth", "yrseduc", 
-                ctrend_1, ctrend_2, #ctrend_3, 
+                ctrend_1, ctrend_2, ctrend_3, 
                 "normchbyear", "w_ch") 
 
 data.share <- 
@@ -287,10 +170,10 @@ z <- as.matrix(data.share[,"t_compschool"])
 # Basic model for actual preliminary analyses 
 x.formula <- as.formula(paste0("~(-1 + factor(country) + factor(chbyear) + factor(sex) + factor(chsex) + factor(int_year) + poly(agemonth,2) + poly(yrseduc,2) + ", 
                                paste(ctrend_1, collapse = " + "), " + ", 
-                               paste(ctrend_2, collapse = " + "), #" + ", 
-                               #paste(ctrend_3, collapse = " + "), 
-                               ")"))
-x <- sparse.model.matrix(x.formula, 
+                               paste(ctrend_2, collapse = " + "), " + ", 
+                               paste(ctrend_3, collapse = " + "), 
+                               ")^2"))
+x <- model.matrix(x.formula, 
                          data=data.share) 
 
 # Start: partialling out -------------------------------------------------------------------------------------
